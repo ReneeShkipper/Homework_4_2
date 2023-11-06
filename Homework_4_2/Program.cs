b8013778576d97764ac343b1ebc8913b8823cb9c
ReneeShkipper <marinkagnome@gmail.com>
2023-11-06 14:06:21 +0300

    Add project files.

---
+++ Homework_4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Write the size of the array");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Fill the array");
+int[] array = new int[N];
+
+for (int i = 0; i < N; i++) array[i] = int.Parse(Console.ReadLine());
+int max = array[0];
+int min = array[0];
+foreach (int j in array)
+{
+    max = j > max ? j : min = j < min ? j : min;
+}
+int sum = max + min;
+Console.WriteLine($"sum = {sum}");
+Console.ReadLine();
