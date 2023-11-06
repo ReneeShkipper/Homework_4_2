8b6a158489f784beca6cb83507a1439676e75d0e
ReneeShkipper <marinkagnome@gmail.com>
2023-11-06 14:38:00 +0300

    Processing of a one-dimensional array into a two-dimensional one

--- Homework_4_2/Program.cs
+++ Homework_4_2/Program.cs
@@ -1,14 +1,21 @@
 ﻿Console.WriteLine("Write the size of the array");
 int N = int.Parse(Console.ReadLine());
+int A = int.Parse(Console.ReadLine());
 Console.WriteLine("Fill the array");
-int[] array = new int[N];
-
-for (int i = 0; i < N; i++) array[i] = int.Parse(Console.ReadLine());
-int max = array[0];
-int min = array[0];
-foreach (int j in array)
+int[,] array = new int[N,A];
+for (int i = 0; i < N; i++)
 {
-    max = j > max ? j : min = j < min ? j : min;
+    for (int j = 0; j < A; j++) 
+    { 
+        array[i,j] = int.Parse(Console.ReadLine());
+    }
+}
+int max = array[0,0];
+int min = array[0,0];
+foreach (int s in array)
+{
+    max = s > max ? s : max;
+    min = s < min ? s : min;
 }
 int sum = max + min;
 Console.WriteLine($"sum = {sum}");
